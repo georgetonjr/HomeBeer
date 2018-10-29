636aeffcecef8567d3d57c96a1c04380ac122a04
georgetonjr <37754623+georgetonjr@users.noreply.github.com>
2018-10-29 10:16:23 -0300

    Att.

---
+++ HomeBeer - Projeto v1.0/cadastro.php
@@ -0,0 +1,17 @@
+<?php
+			if(!empty($_POST['cadastro'])) {
+				$email = pg_escape_string($_POST['email']);
+				$senha = pg_escape_string($_POST['senha']);
+                $apelido = pg_escape_string($_POST['apelido']);
+                $ano_nasc = pg_escape_string($_POST['idade']);
+                $cpf = pg_escape_string($_POST['cpf']);
+				$sql = "INSERT INTO usuario(email, senha, apelido, ano_nasc) VALUES('$email', '$senha', '$apelido', '$idade', '$cpf')";
+				$result = pg_query($sql);
+				if(!$result) {
+					$errormessage = pg_last_error();
+					echo "Erro com a QUERY: ".$errormessage;
+					exit();
+				}
+				pg_close();
+			}
+		?>
